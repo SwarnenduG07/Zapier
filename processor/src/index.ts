3dfe8ff4ce7ffcc3e52553dbf3c9449c089f1be5
Sunny <swarnendughosh05@gmail.com>
2024-09-12 13:02:10 +0530

    kafka added

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,3 +1,30 @@
-while(1) {
-    
+import { PrismaClient } from "@prisma/client";
+import {Kafka} from "kafkajs";
+const TOPIC_NAME = "zap-events ";
+
+const client = new PrismaClient();
+
+const kafka = new Kafka({
+    clientId: "outbox-processor",
+    brokers: ['localhost:9092']
+})
+
+async function main() {
+    const producer = kafka.producer();
+    await producer.connect();
+    while(1) {
+         const pendingRow = await client.zapRunOutBox.findMany({
+            where: {},
+            take: 10
+         })
+             producer.send({
+                topic: TOPIC_NAME,
+                messages: pendingRow.map(r => ({
+                        value: r.zapRunId,
+                    }))
+             });
+    }
 }
+
+main();
+
