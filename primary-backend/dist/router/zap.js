a575bbd7dca41fcd3097c56c9bfcc2a780960f27
Sunny <swarnendughosh05@gmail.com>
2024-09-17 20:35:14 +0530

    meta data added

--- primary-backend/dist/router/zap.js
+++ primary-backend/dist/router/zap.js
@@ -12,17 +12,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.zapRouter = void 0;
 const express_1 = require("express");
 const middleware_1 = require("../middleware");
-const types_1 = require("../types");
 const db_1 = require("../db");
+const types_1 = require("../types");
 const router = (0, express_1.Router)();
 router.post("/", middleware_1.authMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    //@ts-ignore
+    // @ts-ignore
     const id = req.id;
     const body = req.body;
     const parsedData = types_1.ZapSchema.safeParse(body);
     if (!parsedData.success) {
         return res.status(411).json({
-            message: "Incorrect inputs"
+            message: "Incorrect "
         });
     }
     const zapId = yield db_1.prismaClient.$transaction((tx) => __awaiter(void 0, void 0, void 0, function* () {
@@ -33,7 +33,8 @@ router.post("/", middleware_1.authMiddleware, (req, res) => __awaiter(void 0, vo
                 actions: {
                     create: parsedData.data.actions.map((x, index) => ({
                         actionId: x.availableActionId,
-                        sortringOrder: index
+                        sortingOrder: index,
+                        metadata: x.actionMetaData
                     }))
                 }
             }
@@ -41,15 +42,15 @@ router.post("/", middleware_1.authMiddleware, (req, res) => __awaiter(void 0, vo
         const trigger = yield tx.trigger.create({
             data: {
                 triggerId: parsedData.data.availableTriggerId,
-                zapId: zap.id
+                zapId: zap.id,
             }
         });
         yield tx.zap.update({
             where: {
-                id: zap.id,
+                id: zap.id
             },
             data: {
-                triggerId: trigger.id,
+                triggerId: trigger.id
             }
         });
         return zap.id;
@@ -59,21 +60,21 @@ router.post("/", middleware_1.authMiddleware, (req, res) => __awaiter(void 0, vo
     });
 }));
 router.get("/", middleware_1.authMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    //@ts-ignore
+    // @ts-ignore
     const id = req.id;
     const zaps = yield db_1.prismaClient.zap.findMany({
         where: {
-            userId: id,
+            userId: id
         },
         include: {
             actions: {
                 include: {
-                    type: true,
+                    type: true
                 }
             },
             trigger: {
                 include: {
-                    type: true,
+                    type: true
                 }
             }
         }
@@ -86,26 +87,26 @@ router.get("/:zapId", middleware_1.authMiddleware, (req, res) => __awaiter(void 
     //@ts-ignore
     const id = req.id;
     const zapId = req.params.zapId;
-    const zaps = yield db_1.prismaClient.zap.findFirst({
+    const zap = yield db_1.prismaClient.zap.findFirst({
         where: {
             id: zapId,
-            userId: id,
+            userId: id
         },
         include: {
             actions: {
                 include: {
-                    type: true,
+                    type: true
                 }
             },
             trigger: {
                 include: {
-                    type: true,
+                    type: true
                 }
             }
         }
     });
     return res.json({
-        zaps
+        zap
     });
 }));
 exports.zapRouter = router;
--- primary-backend/src/router/zap.ts
+++ primary-backend/src/router/zap.ts
@@ -1,109 +1,116 @@
-import e, { Router } from "express";
+
+import { Router } from "express";
 import { authMiddleware } from "../middleware";
-import { ZapSchema } from "../types";
 import { prismaClient } from "../db";
-import { date, z } from "zod";
+import { ZapSchema } from "../types";
 
 const router = Router();
 
 router.post("/", authMiddleware, async (req, res) => {
-    //@ts-ignore
-    const id  = req.id;
-    const body = req.body; 
+    // @ts-ignore
+    const id: string = req.id;
+    const body = req.body;
     const parsedData = ZapSchema.safeParse(body);
-    if(!parsedData.success) {
-          return res.status(411).json({
-            message: "Incorrect inputs"
-          });
-    }
-    const zapId =  await prismaClient.$transaction(async tx => {
-         const zap = await prismaClient.zap.create({
+    
+    if (!parsedData.success) {
+        return res.status(411).json({
+            message: "Incorrect "
+        });
+    }   
+
+    const zapId = await prismaClient.$transaction(async tx => {
+        const zap = await prismaClient.zap.create({
             data: {
                 userId: parseInt(id),
                 triggerId: "",
                 actions: {
                     create: parsedData.data.actions.map((x, index) => ({
                         actionId: x.availableActionId,
-                        sortringOrder: index
+                        sortingOrder: index,
+                        metadata: x.actionMetaData
                     }))
-                 }
-             }
-    
+                }
+            }
         })
+
         const trigger = await tx.trigger.create({
-            data : {
+            data: {
                 triggerId: parsedData.data.availableTriggerId,
-                zapId: zap.id
+                zapId: zap.id,
             }
         });
+
         await tx.zap.update({
             where: {
-                id: zap.id,
+                id: zap.id
             },
             data: {
-                triggerId: trigger.id,
+                triggerId: trigger.id
             }
         })
-        return zap.id
+
+        return zap.id;
+
     })
-     return res.json({
-         zapId
-     })    
-   
+    return res.json({
+        zapId
+    })
 })
-router.get("/", authMiddleware, async(req, res) => {
+
+router.get("/", authMiddleware, async (req, res) => {
+    // @ts-ignore
+    const id = req.id;
+    const zaps = await prismaClient.zap.findMany({
+        where: {
+            userId: id
+        },
+        include: {
+            actions: {
+               include: {
+                    type: true
+               }
+            },
+            trigger: {
+                include: {
+                    type: true
+                }
+            }
+        }
+    });
+
+    return res.json({
+        zaps
+    })
+})
+
+router.get("/:zapId", authMiddleware, async (req, res) => {
     //@ts-ignore
-   const id = req.id;
-   const zaps = await prismaClient.zap.findMany({
-    where: {
-        userId: id,
-    },
-    include: {
-        actions: {
-            include: {
-                type: true,
-            }
+    const id = req.id;
+    const zapId = req.params.zapId;
+
+    const zap = await prismaClient.zap.findFirst({
+        where: {
+            id: zapId,
+            userId: id
         },
-        trigger: {
-            include: {
-                type: true,
+        include: {
+            actions: {
+               include: {
+                    type: true
+               }
+            },
+            trigger: {
+                include: {
+                    type: true
+                }
             }
         }
-    
-       }
-   })
-   return res.json({
-      zaps
-   })
-})
-router.get("/:zapId", authMiddleware,async (req, res) => {
-     //@ts-ignore
-   const id = req.id;
-   const zapId = req.params.zapId;
-   const zaps = await prismaClient.zap.findFirst({
-    where: {
-        id: zapId,
-        userId: id,
-    },
-    include: {
-        actions: {
-            include: {
-                type: true,
-            }
-        },
-        trigger: {
-            include: {
-                type: true,
-            }
-        }
-    
-       }
-   })
-   return res.json({
-     zaps
-   })
+    });
+
+    return res.json({
+        zap
+    })
 
 })
-
 
 export const zapRouter = router;
