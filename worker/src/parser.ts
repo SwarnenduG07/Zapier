6e62fc9d7ec128559ad2124c73183b7ebdf2a82a
Sunny <swarnendughosh05@gmail.com>
2024-09-21 11:32:18 +0530

    parser added

---
+++ worker/src/parser.ts
@@ -0,0 +1,36 @@
+export function Parse(text: string,values: any ,startDelimeter = "{", endDelimeter = "}") {
+    let startIndex = 0;
+    let endIndex =  1;
+ 
+     let finalString  = "";
+    while (endIndex != text.length) {
+        if(text[startIndex] === startDelimeter)  {
+            let endPoint = startIndex + 2;
+            while (text[endPoint] !== endDelimeter) {
+                endPoint++;
+            }
+            let strigHoldingValue = text.slice(startIndex +1, endPoint);
+            const key = strigHoldingValue.split(".");
+            let localValue =  {
+                ...values
+            }
+            for (let i = 0; i< key.length; i++) {
+                if(typeof localValue == "string") {
+                    localValue = JSON.parse(localValue)
+                }
+                localValue = localValue[key[i]];
+            }
+            finalString += localValue;
+            startIndex = endPoint + 1;
+            endIndex = endIndex +2;
+        } else {
+            finalString += text[startIndex];
+            startIndex++;
+            endIndex++;
+        }
+    }
+    if(text[startIndex]) {
+        finalString += text[startIndex];
+    }
+    return finalString;
+}
