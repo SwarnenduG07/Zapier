21ecec31ec0f93f9da940177f372160ec5f44172
Sunny <swarnendughosh05@gmail.com>
2024-09-23 16:17:53 +0530

    solana Reconceliation added

--- worker/src/solana.ts
+++ worker/src/solana.ts
@@ -1,12 +1,32 @@
 
+import { PrismaClient } from "@prisma/client";
 import { Keypair, LAMPORTS_PER_SOL, SystemProgram, Transaction, PublicKey, sendAndConfirmTransaction, Connection } from "@solana/web3.js";
 import base58 from "bs58";
+import { log } from "console";
+
+const prismaCLient = new PrismaClient();
 
 const connection = new Connection("https://api.mainnet-beta.solana.com", "finalized");
 
-export async function sendSol(to: string, amount: string) {
+export async function sendSol(zapRunId: string , to: string, amount: string) {
     const keypair = Keypair.fromSecretKey(base58.decode((process.env.SOL_PRIVATE_KEY ?? "")))
     console.log(keypair.publicKey);
+  
+    const existingTransaction = await prismaCLient.transaction.findFirst({
+        where: {
+          zapRunId,
+          type: "send-sol",
+        }
+    });
+    if (existingTransaction) {
+       const txnStatus = await connection.getSignatureStatus(existingTransaction.txSignature);
+       if(txnStatus && txnStatus.value?.confirmationStatus === "finalized") {
+        console.log(`Transaction for zaprunID ${zapRunId} is already confremed`);
+        return;
+       } else {
+        console.log(`Trasaction of zaprunId ${zapRunId} is pending or field. reattepting...`);
+       }
+    }
     const transferTransaction = new Transaction().add(
         SystemProgram.transfer({
           fromPubkey: keypair.publicKey,
@@ -14,8 +34,24 @@ export async function sendSol(to: string, amount: string) {
           lamports: parseFloat(amount) * LAMPORTS_PER_SOL, // 0.1 => 10 ^ 8
         })
     );
+     
+    try {
+       const signature = await sendAndConfirmTransaction(connection, transferTransaction, [keypair]);
+      console.log("sol Sent!")
+      await prismaCLient.transaction.create({
+        data: {
+          zapRunId,
+          txSignature: signature,
+          type: "send-sol",
+          status: "panding",
+        }
+      });
+      console.log("SOl send! Trasaction Signature : ", signature);
+      
+    } catch (e: any) {
+       console.log("Error while sending Sol", e);
+       
+    }
 
-    await sendAndConfirmTransaction(connection, transferTransaction, [keypair]);
-    console.log("sol Sent!")
 
 }
